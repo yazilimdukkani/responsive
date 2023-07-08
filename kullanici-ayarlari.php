354db8198b649c134d9500f178212b5d0021a855
Baha Aktaş <133813921+yazilimdukkani@users.noreply.github.com>
2023-07-08 09:39:14 +0300

    forum-responsive-template

---
+++ kullanici-ayarlari.php
@@ -0,0 +1,53 @@
+<?php 
+
+include("../header.php");
+?>
+<style>
+<?php 
+include("../settings/kullanici.css");
+
+?>
+</style>
+<div class="article">
+
+<div class="article1">
+<!-- new konu -->
+<div class="new-subject" style="float:right;">
+  
+
+</div>
+
+
+ <div class="forum"><i class="fa-brands fa-gripfire"></i>Profili Güncelle</div>
+<div class="forum1">
+
+
+<!-- form elamanları kodlar buraya-->
+
+<div class="form-kapsayici">
+    
+<form>
+    <input type="text" name="discord" placeholder="discord" class="link">
+    <br>
+    <input type="text" name="facebook" placeholder="facebook" class="link">
+    <br>
+    <input type="text" name="instagram" placeholder="instagram" class="link">
+    <br>
+    <input type="text" name="linkedin" placeholder="linkedin" class="link">
+    <br>
+    <input type="text" name="email" placeholder="email" class="link"><br>
+    <button type="submit" class="buton-profil">Profili Güncelle</button>
+</form>
+
+
+</div>
+
+<!-- form elamanları kodlar buraya-->
+
+</div>
+</div>
+</div>
+
+<?php 
+include("../footer.php");
+?>
